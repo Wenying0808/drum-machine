aa2ea5a265776c8bc3bac5d5217f46614766131a
Wenying Chang <changwenyingnl@gmail.com>
2024-03-09 08:21:36 +0100

    play audio

--- src/keyButton/keyButton.jsx
+++ src/keyButton/keyButton.jsx
@@ -1,37 +1,17 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import './keyButton.css'
 
 const KeyButton = ({label, url, disabled, onClick}) => {
 
     const [pressed, setPressed] = useState(false);
-
-    const playAudio = useCallback(() => {
-        if (url) {
-            const audio = new Audio(url);
-            audio.play();
-            console.log('audio should be played');
-          }
-    },[url]);
-
-    const handleClick = useCallback(() => {
-        if (!disabled && onClick) {
-          onClick();
-          playAudio();
-        }
-    },[disabled, onClick, playAudio]);
-
-    
-    
     
     
     useEffect( () => {
         const handleKeyDown = (event) => {
             if (event.key.toUpperCase() === label && !disabled){
                 setPressed(true);
-                handleClick();
                 console.log('keydown', event.key);
             }
-            
         };
     
         const handleKeyUp = () => {
@@ -48,15 +28,14 @@ const KeyButton = ({label, url, disabled, onClick}) => {
             window.removeEventListener('keyup', handleKeyUp);
         };
 
-    },[label, disabled, handleClick]);
+    },[label, disabled]);
+  
       
     return(
         <div 
             className={`keybutton ${disabled ? 'disabled' : 'enabled' } ${pressed ? 'active' : ''}`}
-            onClick={handleClick}
-            
+            onClick={onClick}   
         >
-            <audio className="clip" src={url} />
             {label}
         </div>
     );
